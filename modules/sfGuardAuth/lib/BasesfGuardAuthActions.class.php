5dfdd0aae70e1b92a99ca8b56c95f54aca4e019b
Romain Pierlot <rpierlot-external@etsglobal.org>
2015-07-21 13:09:00 +0200

    [EB-3621] added automatic redirection to sso

--- modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
+++ modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
@@ -32,48 +32,51 @@ class BasesfGuardAuthActions extends sfActions
     if ($user->isAuthenticated())
     {
       return $this->redirect('@homepage');
+    } else {
+      $user = $this->ssoLogin();
+      $this->getUser()->signin($user, true);
+      $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url');
+
+      return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
     }
+  }
 
-    $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin'); 
-    $this->form = new $class();
+  private function ssoLogin()
+  {
+    $ssoProvider = new SsoProvider();
+    if (!isset($_GET['code'])) {
 
-    if ($request->isMethod('post'))
-    {
-      $this->form->bind($request->getParameter('signin'));
-      if ($this->form->isValid())
-      {
-        $values = $this->form->getValues(); 
-        $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
+      // If we don't have an authorization code then get one
+      $authUrl = $ssoProvider->getAuthorizationUrl();
+      header('Location: '.$authUrl);
+      exit;
 
-        // always redirect to a URL set in app.yml
-        // or to the referer
-        // or to the homepage
-        $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
+// Check given state against previously stored one to mitigate CSRF attack
+    } else {
 
-        return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
+      // Try to get an access token (using the authorization code grant)
+      $token = $ssoProvider->getAccessToken('authorization_code', [
+          'code' => $_GET['code']
+      ]);
+
+      // Optional: Now you have a token you can look up a users profile data
+      try {
+        $userDetails =  $ssoProvider->getUserDetails(new \League\OAuth2\Client\Token\AccessToken(['access_token' => $token->accessToken]));
+        $user = Doctrine_Core::getTable('sfGuardUser')->createQuery()
+            ->select()
+            ->where('username = ?', $userDetails->username)
+            ->execute()->getFirst();
+
+        $user->setAccessToken($token->accessToken);
+        $user->save();
+        return $user;
+
+      } catch (Exception $e) {
+
+        error_log(sprintf('Cannot find user with access token %s', $e->getMessage()));
+
+        throw $e;
       }
-    }
-    else
-    {
-      if ($request->isXmlHttpRequest())
-      {
-        $this->getResponse()->setHeaderOnly(true);
-        $this->getResponse()->setStatusCode(401);
-
-        return sfView::NONE;
-      }
-
-      // if we have been forwarded, then the referer is the current URL
-      // if not, this is the referer of the current request
-      $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
-
-      $module = sfConfig::get('sf_login_module');
-      if ($this->getModuleName() != $module)
-      {
-        return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
-      }
-
-      $this->getResponse()->setStatusCode(401);
     }
   }
 
