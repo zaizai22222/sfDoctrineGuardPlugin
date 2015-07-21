7851d0dddd472d39d275b28f204538489278300e
Romain Pierlot <rpierlot-external@etsglobal.org>
2015-07-21 13:16:00 +0200

    [EB-3621] fixed merge

--- modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
+++ modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
@@ -19,15 +19,6 @@ class BasesfGuardAuthActions extends sfActions
 {
   public function executeSignin($request)
   {
-    if (isset($_GET['login']) && $_GET['login'] == 'new')
-    {
-      $user = $this->ssoLogin();
-      $this->getUser()->signin($user, true);
-      $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url');
-
-      return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
-
-    }
     $user = $this->getUser();
     if ($user->isAuthenticated())
     {
@@ -38,45 +29,6 @@ class BasesfGuardAuthActions extends sfActions
       $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url');
 
       return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
-    }
-  }
-
-  private function ssoLogin()
-  {
-    $ssoProvider = new SsoProvider();
-    if (!isset($_GET['code'])) {
-
-      // If we don't have an authorization code then get one
-      $authUrl = $ssoProvider->getAuthorizationUrl();
-      header('Location: '.$authUrl);
-      exit;
-
-// Check given state against previously stored one to mitigate CSRF attack
-    } else {
-
-      // Try to get an access token (using the authorization code grant)
-      $token = $ssoProvider->getAccessToken('authorization_code', [
-          'code' => $_GET['code']
-      ]);
-
-      // Optional: Now you have a token you can look up a users profile data
-      try {
-        $userDetails =  $ssoProvider->getUserDetails(new \League\OAuth2\Client\Token\AccessToken(['access_token' => $token->accessToken]));
-        $user = Doctrine_Core::getTable('sfGuardUser')->createQuery()
-            ->select()
-            ->where('username = ?', $userDetails->username)
-            ->execute()->getFirst();
-
-        $user->setAccessToken($token->accessToken);
-        $user->save();
-        return $user;
-
-      } catch (Exception $e) {
-
-        error_log(sprintf('Cannot find user with access token %s', $e->getMessage()));
-
-        throw $e;
-      }
     }
   }
 
