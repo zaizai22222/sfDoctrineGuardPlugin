5154b66f964dea85c095daa163cf5342dca58873
Joseph Rouff <rouffj@gmail.com>
2016-01-04 18:21:20 +0100

    Re-enable form login

--- modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
+++ modules/sfGuardAuth/lib/BasesfGuardAuthActions.class.php
@@ -22,61 +22,21 @@ class BasesfGuardAuthActions extends sfActions
   public function executeSignin($request)
   {
     $user = $this->getUser();
-    if ($user->isAuthenticated())
-    {
+    if ($user->isAuthenticated()) {
       return $this->redirect('@homepage');
-    } else {
+    }
+
+    if (sfConfig::get('app_features_sso')) {
       $user = $this->ssoLogin();
       $this->getUser()->signin($user, true);
       $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url');
 
       return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
     }
-  }
 
-  private function ssoLogin()
-  {
-    $ssoProvider = new SsoProvider();
-    if (!isset($_GET['code'])) {
+    $redirectUrl = $this->formLogin($request);
 
-      // If we don't have an authorization code then get one
-      $authUrl = $ssoProvider->getAuthorizationUrl();
-      header('Location: '.$authUrl);
-      exit;
-
-// Check given state against previously stored one to mitigate CSRF attack
-    } else {
-
-      // Try to get an access token (using the authorization code grant)
-      $token = $ssoProvider->getAccessToken('authorization_code', [
-          'code' => $_GET['code']
-      ]);
-
-      // Optional: Now you have a token you can look up a users profile data
-      try {
-        $userDetails =  $ssoProvider->getUserDetails(new \League\OAuth2\Client\Token\AccessToken(['access_token' => $token->accessToken]));
-        $user = Doctrine_Core::getTable('sfGuardUser')->createQuery()
-            ->select()
-            ->where('username = ?', $userDetails->username)
-            ->execute()->getFirst();
-
-
-        $currentDate = new \DateTime();
-        $expirationDate = clone $currentDate;
-        $expirationDate->add(new \DateInterval('PT'.self::ACCESS_TOKEN_DURATION.'S'));
-
-        $user->setAccessToken($token->accessToken);
-        $user->setAccessTokenExpirationDate($expirationDate->format('Y-m-d H:i:s'));
-        $user->save();
-        return $user;
-
-      } catch (Exception $e) {
-
-        error_log(sprintf('Cannot find user with access token %s', $e->getMessage()));
-
-        throw $e;
-      }
-    }
+    return $this->redirect($redirectUrl);
   }
 
   public function executeSignout($request)
@@ -109,4 +69,88 @@ class BasesfGuardAuthActions extends sfActions
   {
     throw new sfException('This method is not yet implemented.');
   }
+
+  private function formLogin($request)
+  {
+     $class = sfConfig::get('app_sf_guard_plugin_signin_form', 'sfGuardFormSignin');
+     $this->form = new $class();
+
+     if ($request->isMethod('post')) {
+       $this->form->bind($request->getParameter('signin'));
+       if ($this->form->isValid()) {
+         $values = $this->form->getValues();
+         $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
+
+         // always redirect to a URL set in app.yml
+         // or to the referer
+         // or to the homepage
+         $signinUrl = sfConfig::get('app_sf_guard_plugin_success_signin_url', $user->getReferer($request->getReferer()));
+
+         return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
+       }
+     }
+     else {
+       if ($request->isXmlHttpRequest()) {
+         $this->getResponse()->setHeaderOnly(true);
+         $this->getResponse()->setStatusCode(401);
+
+         return sfView::NONE;
+       }
+
+       // if we have been forwarded, then the referer is the current URL
+       // if not, this is the referer of the current request
+       $user->setReferer($this->getContext()->getActionStack()->getSize() > 1 ? $request->getUri() : $request->getReferer());
+
+       $module = sfConfig::get('sf_login_module');
+       if ($this->getModuleName() != $module) {
+         return $this->redirect($module.'/'.sfConfig::get('sf_login_action'));
+       }
+
+       $this->getResponse()->setStatusCode(401);
+      }
+  }
+
+  private function ssoLogin()
+  {
+    $ssoProvider = new SsoProvider();
+    if (!isset($_GET['code'])) {
+
+      // If we don't have an authorization code then get one
+      $authUrl = $ssoProvider->getAuthorizationUrl();
+      header('Location: '.$authUrl);
+      exit;
+
+    } else {
+      // Try to get an access token (using the authorization code grant)
+      $token = $ssoProvider->getAccessToken('authorization_code', [
+          'code' => $_GET['code']
+      ]);
+
+      // Optional: Now you have a token you can look up a users profile data
+      try {
+        $userDetails =  $ssoProvider->getUserDetails(new \League\OAuth2\Client\Token\AccessToken(['access_token' => $token->accessToken]));
+        $user = Doctrine_Core::getTable('sfGuardUser')->createQuery()
+            ->select()
+            ->where('username = ?', $userDetails->username)
+            ->execute()->getFirst();
+
+
+        $currentDate = new \DateTime();
+        $expirationDate = clone $currentDate;
+        $expirationDate->add(new \DateInterval('PT'.self::ACCESS_TOKEN_DURATION.'S'));
+
+        $user->setAccessToken($token->accessToken);
+        $user->setAccessTokenExpirationDate($expirationDate->format('Y-m-d H:i:s'));
+        $user->save();
+        return $user;
+
+      } catch (Exception $e) {
+
+        error_log(sprintf('Cannot find user with access token %s', $e->getMessage()));
+
+        throw $e;
+      }
+    }
+  }
+
 }
